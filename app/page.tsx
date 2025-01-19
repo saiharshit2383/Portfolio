94306e15f5a8cdb09ba0616770cc52610b2cc754
saiharshit2383 <saiharshit360@gmail.com>
2025-01-19 12:11:56 +0530

    Harshit's Portfolio

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import dynamic from 'next/dynamic';
+import { navItems } from "@/data";
+
+const Hero = dynamic(() => import('@/components/Hero'), { ssr: false });
+const Grid = dynamic(() => import('@/components/Grid'), { ssr: false });
+const Footer = dynamic(() => import('@/components/Footer'), { ssr: false });
+const Approach = dynamic(() => import('@/components/Approach'), { ssr: false });
+const Experience = dynamic(() => import('@/components/Experience'), { ssr: false });
+const RecentProjects = dynamic(() => import('@/components/RecentProjects'), { ssr: false });
+const FloatingNav = dynamic(() => import('@/components/ui/FloatingNavbar').then(mod => mod.FloatingNav), { ssr: false });
+
+
+
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
